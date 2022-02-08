b2db97e3068390f080ad6f64e99b4356cf0288b6
Justin Hong <abigsmall@gmail.com>
2022-02-08 01:44:22 -0800

    Add shoe grid styles

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -53,6 +53,12 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex: 1;
+  min-width: 100%;
+
+  @media (min-width: 936px) {
+    min-width: 278px;
+  }
 `;
 
 const Wrapper = styled.article``;
@@ -61,7 +67,9 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
